2161f0ec72ea55002d59c8a0276a2bacde8f30fc
dariofusco <fdario@hotmail.it>
2023-09-26 15:32:11 +0200

    stampa card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,29 @@
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
+
 <body>
-    <h1 class="text-center">Hello World</h1>
+
+    <div class="container">
+        <h1 class="text-center">Movies</h1>
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-4 py-3">
+                    <div class="card" style="width: 18rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: {{ $movie->id }}</li>
+                            <li class="list-group-item">Titolo: {{ $movie->title }}</li>
+                            <li class="list-group-item">Titolo Originale: {{ $movie->original_title }}</li>
+                            <li class="list-group-item">Nazionalità: {{ $movie->nationality }}</li>
+                            <li class="list-group-item">Data: {{ $movie->date }}</li>
+                            <li class="list-group-item">Voto: {{ $movie->vote }}</li>
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
 </body>
+
 </html>
